ce094f4750ceffb6c9f6638f86fbd7e51269fc1c
Patrick Martin <patrickmmartin@gmail.com>
2011-11-29 10:17:28 +0000

    * fix long-running typo * add comment

--- build-egg.bat
+++ build-egg.bat
@@ -1,5 +1,6 @@
 if exist build rmdir /S /Q build
 if exist dist rmdir /S /Q dist
-if exist TracIndexServer.egg-info build rmdir /S /Q TracIndexServer.egg-info
+if exist TracIndexServer.egg-info rmdir /S /Q TracIndexServer.egg-info
 
+:: assuming the python location
 c:\python27\python setup.py bdist_egg
