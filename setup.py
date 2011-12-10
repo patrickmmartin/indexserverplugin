d0c665ceac29010f6006a2a9034d37743656499d
Patrick Martin <patrickmmartin@gmail.com>
2011-12-10 15:28:08 +0000

    adding some minimal embedded custom commands to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2006 Patrick Martin patrickmmartin@gmail.com 
+# Copyright (C) 2006-2011 Patrick Martin patrickmmartin@gmail.com 
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
@@ -8,14 +8,58 @@
 
 from setuptools import setup, find_packages
 
+from distutils.cmd import Command
+from distutils.errors import *
+
+class CustomCommand(Command):
+    user_options = []
+    def initialize_options(self):
+		None;
+    def finalize_options(self):
+		None;
+
+class CleanCommand(Command):
+    description = "custom clean command that forcefully removes dist/build directories"
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        os.system('rm -rf ./build ./dist')		
+
+		
+class DeployCommand(CustomCommand):
+    description = "custom command to update a Trac environment"
+    def run(self):
+		# TODO assert we have a valid location
+		print 'deploying the build ...'
+		# TODO copy the build to the environment plugins location
+		print 'deploy done'
+
+class BounceCommand(CustomCommand):
+    description = "custom command to bounce Apache"
+    def run(self):
+		# execute the bounce command
+		print 'bouncing the server ...'
+		# TODO execute the relevant apache graceful restart
+		print 'bounce done'
+
+		
+		
 setup(
     name = 'TracIndexServer', version = '0.2',
     author = 'Patrick Martin', author_email = 'patrickmmartin@gmail.com',
-    url = 'about:mozilla',
-    description = 'Support for Using MS Index Server in Trac for full-text repository search',
+    url = 'https://github.com/patrickmmartin',
+    description = 'Adds MS Index Server to Trac environment search',
+	long_description = 'Adds Support for using MS Index Server in Trac environments for full-text repository search.',
     license = 'BSD',
     packages = find_packages(exclude=['*.tests*']),
+	cmdclass = {'clean': CleanCommand, 'deploy': DeployCommand, 'bounce': BounceCommand},
     entry_points = {
-        'trac.plugins': ['tracindexserversearch = tracindexserversearch']
+        'trac.plugins': ['tracindexserversearch = tracindexserversearch'],
+        ],		
     }
+
 )
+
