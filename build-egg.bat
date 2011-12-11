7f2dc6a0ea9ac7e766aa6c021d3e09bbd4068ff4
Patrick Martin <patrickmmartin@gmail.com>
2011-12-11 14:34:14 +0000

    cleaning up redundant scripts

--- build-egg.bat
+++
@@ -1,6 +0,0 @@
-if exist build rmdir /S /Q build
-if exist dist rmdir /S /Q dist
-if exist TracIndexServer.egg-info rmdir /S /Q TracIndexServer.egg-info
-
-:: assuming the python location
-c:\python27\python setup.py bdist_egg
