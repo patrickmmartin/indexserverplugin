21bc61fee7b38f46e42dcf96256a897ec66157e9
Patrick Martin <patrickmmartin@gmail.com>
2015-03-15 13:05:45 +0000

    autopep8 on file

--- tracindexserversearch/indexserversearch.py
+++ tracindexserversearch/indexserversearch.py
@@ -4,7 +4,7 @@
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution. 
+# you should have received as part of this distribution.
 
 from trac.core import *
 from trac.search import ISearchSource, shorten_result
@@ -13,181 +13,187 @@ from trac.mimeview.api import Mimeview
 import win32com.client
 from pythoncom import CoInitialize, CoUninitialize, com_error
 
-com= win32com.client.Dispatch
-con= win32com.client.constants
+com = win32com.client.Dispatch
+con = win32com.client.constants
 
 __all__ = ['IndexServerProxy']
 
+
 class IndexServerProxy(Component):
-  """Component to pass on queries to the Index Server components."""   
-  implements(ISearchSource)
 
-  def get_index_server_results(self, repo, wordlist):
-    
-      if not self.env.config.get('indexserver-search', 'catalogue-root', ''):
-        raise TracError("Index Server plugin is not " \
-                "configured correctly. Set the 'catalogue-root' option " \
-                "under the 'indexserver-search' section to the full " \
-                "(case-correct) path of the root of the catalogued source.")
+    """Component to pass on queries to the Index Server components."""
+    implements(ISearchSource)
 
-      #this sets up a handler for unicode conversion
-      # ? is this more efficient outside the loop?
-      to_unicode = Mimeview(self.env).to_unicode        
+    def get_index_server_results(self, repo, wordlist):
 
-      checkoutroot = self.env.config.get('indexserver-search', 'catalogue-root', '')
-      CoInitialize()
-      try:
-        fso = com("Scripting.FileSystemObject")
-        if not fso.FolderExists(checkoutroot):
-          raise TracError("Index Server plugin is not " \
-                  "configured correctly. The folder referred to in the 'catalogue-root' option '" 
-                  + checkoutroot + 
-                  "' under the 'indexserver-search' section does not exist.")
-        
-        query = com('IXSSO.Query')
+        if not self.env.config.get('indexserver-search', 'catalogue-root', ''):
+            raise TracError("Index Server plugin is not "
+                            "configured correctly. Set the 'catalogue-root' option "
+                            "under the 'indexserver-search' section to the full "
+                            "(case-correct) path of the root of the catalogued source.")
 
-        query.Dialect = 2
-        query.Columns = "rank, filename, path, characterization"
-        phrase   = ' '.join(wordlist)
+        # this sets up a handler for unicode conversion
+        # ? is this more efficient outside the loop?
+        to_unicode = Mimeview(self.env).to_unicode
 
-        querystr = phrase
-        querystr += " AND NOT {prop name=path}{regex}*\.svn*{/regex}"
-
-        query.Query = querystr;
-
-        query.SortBy = "rank[d]"  
-        query.Catalog = "Source Catalogue"
-        query.OptimizeFor      = "recall"
-        query.MaxRecords       = 100
-
-        self.env.log.debug("index server query is: " + querystr )
+        checkoutroot = self.env.config.get(
+            'indexserver-search', 'catalogue-root', '')
+        CoInitialize()
         try:
-          result = query.CreateRecordset("sequential")
+            fso = com("Scripting.FileSystemObject")
+            if not fso.FolderExists(checkoutroot):
+                raise TracError("Index Server plugin is not "
+                                "configured correctly. The folder referred to in the 'catalogue-root' option '"
+                                + checkoutroot +
+                                "' under the 'indexserver-search' section does not exist.")
 
-        except com_error, (hr, msg, exc, arg):
-          if exc is None:
-            print "There is no extended error information"
-            raise TracError("Index Server raised exception on query [" + phrase + "] " \
-                              " details: %d: %s" % (hr, msg) +
-                              "note that the $ and # characters need to be escaped where not used as special identifiers. " \
-                              "See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/indexsrv/html/ixarch_1n5f.asp " \
-                              " for more details.")
-          else:
-            wcode, source, text, helpFile, helpId, scode = exc
+            query = com('IXSSO.Query')
 
-            raise TracError("Index Server raised exception on query [" + phrase + "] " \
-                              " details: " \
-                              "the source of the error is '" + source + "' " +
-                              "the error message is '" + text + "' " +
-##                              "more info can be found in %s (id=%d) " % (helpFile, helpId) +
-                              "note that the $ and # characters need to be escaped where not used as special identifiers. " \
-                              "See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/indexsrv/html/ixarch_1n5f.asp " \
-                              " for more details.")
-            
- 	
-                    
-        query = None
+            query.Dialect = 2
+            query.Columns = "rank, filename, path, characterization"
+            phrase = ' '.join(wordlist)
 
-        hits = list()
+            querystr = phrase
+            querystr += " AND NOT {prop name=path}{regex}*\.svn*{/regex}"
 
+            query.Query = querystr
 
-        while not (result.EOF):
-            # note: path in search results is lower cased
-            indexpath = to_unicode(result.Fields('path'), "utf_16")
-            # index might not be up completely up to date, and has old files
-            if fso.FileExists(indexpath):
-              # need to get correctly cased file name, and convert into respository URL
-              # this filename should have the desired case
-              fo = fso.GetFile(indexpath)
-              path = to_unicode(fo.Path, "utf_16")
-              filename = to_unicode(fo.Name, "utf_16")
-              fo = None
-              # strip checkout root folder
-              url = path.replace(checkoutroot, "")
-              # and URL-ise
-              url = url.replace("\\", '/')
+            query.SortBy = "rank[d]"
+            query.Catalog = "Source Catalogue"
+            query.OptimizeFor = "recall"
+            query.MaxRecords = 100
 
-              # attempt to get the repository node relating to the url
-              # this may fail if the source index is out of date,
-              # which means the hit may not be present at that url,
-              # so skip these exceptions
-              node = None
-              try:
-                node = repo.get_node(url)
-                change = repo.get_changeset(node.rev)
-              except TracError, e:
-                self.env.log.warning('ignoring failure to obtain node at ' + url + ': ' + e)
+            self.env.log.debug("index server query is: " + querystr)
+            try:
+                result = query.CreateRecordset("sequential")
 
-              # got up to date node - get the details
-              if node:    
-                # attempt to use index server "characterization" field
-                # for files set as application/octet-stream, if present
-                # as index server indexes Office documents,
-                # should pick up abstract of those binary files OK
-                content = None
-                characterization = result.Fields('characterization')
-                if ((node.get_content_type() == "application/octet-stream") and (characterization <> None)):
-                  try:
-                    content = to_unicode(characterization, "utf_16")
-                    self.env.log.debug('got characterization field for node ' + url)
-                  except UnicodeEncodeError, e:
-                    content = None
-                    self.env.log.warning(e)
-                    self.env.log.warning('ignoring failure for conversion of characterization field for node ' + url)
-                                      
-                # if not binary or no abstract, or there was a UnicodeEncodeError ?!
-                # pull back the node content
-                if not content:    
-                  contentstream = node.get_content()
-                  if contentstream:
-                    content = to_unicode(contentstream.read(), node.get_content_type())
-                    self.env.log.debug('got content from repository for node ' + url)
-                  else:
-                    self.env.log.debug('no content from repository for node ' + url)                  
-                            
-                hit = list()
-                  
-                hit.append(self.env.href.browser(url))
-                hit.append(filename + ": " + url)
-                hit.append(change.date)
-                hit.append(change.author)
-                hit.append(shorten_result(content, wordlist))
-                hits.append(hit)
-                 
-                result.MoveNext()
-                
-        
-        result = None
-        fso = None
-        return hits
+            except com_error, (hr, msg, exc, arg):
+                if exc is None:
+                    print "There is no extended error information"
+                    raise TracError("Index Server raised exception on query [" + phrase + "] "
+                                    " details: %d: %s" % (hr, msg) +
+                                    "note that the $ and # characters need to be escaped where not used as special identifiers. "
+                                    "See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/indexsrv/html/ixarch_1n5f.asp "
+                                    " for more details.")
+                else:
+                    wcode, source, text, helpFile, helpId, scode = exc
 
-      # attempt clean-up under all circumstances
-      finally:
-        CoUninitialize()
+                    raise TracError("Index Server raised exception on query [" + phrase + "] "
+                                    " details: "
+                                    "the source of the error is '" + source + "' " +
+                                    "the error message is '" + text + "' " +
+                                    # "more info can be found in %s (id=%d) " % (helpFile, helpId) +
+                                    "note that the $ and # characters need to be escaped where not used as special identifiers. " \
+                                    "See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/indexsrv/html/ixarch_1n5f.asp " \
+                                    " for more details.")
+
+            query = None
+
+            hits = list()
+
+            while not (result.EOF):
+                    # note: path in search results is lower cased
+                indexpath = to_unicode(result.Fields('path'), "utf_16")
+                # index might not be up completely up to date, and has old
+                # files
+                if fso.FileExists(indexpath):
+                    # need to get correctly cased file name, and convert into respository URL
+                    # this filename should have the desired case
+                    fo = fso.GetFile(indexpath)
+                    path = to_unicode(fo.Path, "utf_16")
+                    filename = to_unicode(fo.Name, "utf_16")
+                    fo = None
+                    # strip checkout root folder
+                    url = path.replace(checkoutroot, "")
+                    # and URL-ise
+                    url = url.replace("\\", '/')
+
+                    # attempt to get the repository node relating to the url
+                    # this may fail if the source index is out of date,
+                    # which means the hit may not be present at that url,
+                    # so skip these exceptions
+                    node = None
+                    try:
+                        node = repo.get_node(url)
+                        change = repo.get_changeset(node.rev)
+                    except TracError, e:
+                        self.env.log.warning(
+                            'ignoring failure to obtain node at ' + url + ': ' + e)
+
+                    # got up to date node - get the details
+                    if node:
+                        # attempt to use index server "characterization" field
+                        # for files set as application/octet-stream, if present
+                        # as index server indexes Office documents,
+                        # should pick up abstract of those binary files OK
+                        content = None
+                        characterization = result.Fields('characterization')
+                        if ((node.get_content_type() == "application/octet-stream") and (characterization <> None)):
+                            try:
+                                content = to_unicode(
+                                    characterization, "utf_16")
+                                self.env.log.debug(
+                                    'got characterization field for node ' + url)
+                            except UnicodeEncodeError, e:
+                                content = None
+                                self.env.log.warning(e)
+                                self.env.log.warning(
+                                    'ignoring failure for conversion of characterization field for node ' + url)
+
+                        # if not binary or no abstract, or there was a UnicodeEncodeError ?!
+                        # pull back the node content
+                        if not content:
+                            contentstream = node.get_content()
+                            if contentstream:
+                                content = to_unicode(
+                                    contentstream.read(), node.get_content_type())
+                                self.env.log.debug(
+                                    'got content from repository for node ' + url)
+                            else:
+                                self.env.log.debug(
+                                    'no content from repository for node ' + url)
+
+                        hit = list()
+
+                        hit.append(self.env.href.browser(url))
+                        hit.append(filename + ": " + url)
+                        hit.append(change.date)
+                        hit.append(change.author)
+                        hit.append(shorten_result(content, wordlist))
+                        hits.append(hit)
+
+                        result.MoveNext()
+
+            result = None
+            fso = None
+            return hits
+
+        # attempt clean-up under all circumstances
+        finally:
+            CoUninitialize()
 
 
 # ISearchSource methods
 
-##         The events returned by this function must be tuples of the form 
-##         (internalname, friendlyname, onbydefault). 
-  
-  def get_search_filters(self, req):
-  """Yield the new search item implemented by this class."""   
-      # this Component will support an "Index Server" search
-      yield ('indexserver', 'Index Server', 1)
+# The events returned by this function must be tuples of the form
+# (internalname, friendlyname, onbydefault).
 
-##         The events returned by this function must be tuples of the form 
-##         (href, title, date, author, excerpt). 
+    def get_search_filters(self, req):
+    """Yield the new search item implemented by this class."""
+    # this Component will support an "Index Server" search
+        yield ('indexserver', 'Index Server', 1)
 
-  def get_search_results(self, req, query, filters):
-  """Yield the search results returned from this query."""   
-    if 'indexserver' not in filters:
-      return
-    repo = self.env.get_repository(req.authname)
-    if not isinstance(query, list):
-      query = query.split()
-    query = [q.lower() for q in query]
-    
-    for result in self.get_index_server_results(repo, query):
-      yield result
+# The events returned by this function must be tuples of the form
+# (href, title, date, author, excerpt).
 
+    def get_search_results(self, req, query, filters):
+    """Yield the search results returned from this query."""
+        if 'indexserver' not in filters:
+            return
+        repo = self.env.get_repository(req.authname)
+        if not isinstance(query, list):
+            query = query.split()
+        query = [q.lower() for q in query]
+
+        for result in self.get_index_server_results(repo, query):
+            yield result
